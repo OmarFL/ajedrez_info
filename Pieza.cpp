92c7a7fc1ac8a4587f1557d5a6de2515f5b5f569
Gallego1502 <80354497+Gallego1502@users.noreply.github.com>
2025-05-08 15:43:34 +0200

    Create Pieza.cpp

---
+++ Pieza.cpp
@@ -0,0 +1,33 @@
+#include "Pieza.h"
+#include "freeglut.h"
+
+//Funcion para realizar el polimorfismo con las funciones de dibuja de las distintas piezas
+void Pieza::dibuja() {
+	glColor3f(0.5f, 0.5f, 0.5f); // Color gris
+	glutSolidCube(1.0);
+}
+
+
+void Pieza::setPos(int _f, int _c) {
+	fc.setCasillas(_f, _c);
+}
+
+int Pieza::getTipoPieza() {
+	return 0;
+}
+
+int Pieza::getColor() {
+	return color;
+}
+
+int Pieza::getfila() {
+	return fc.getF();
+}
+
+int Pieza::getcolumna() {
+	return fc.getC();
+}
+
+Casilla Pieza::getCasilla() {
+	return fc;
+}
