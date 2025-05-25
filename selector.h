2d63b1428723802e22ca8dbc99ab73eb23e8a5f1
OmarFL <80384332+OmarFL@users.noreply.github.com>
2025-05-25 22:23:53 +0200

    Create selector.h
    Clase selector para las fichas del tablero

---
+++ selector.h
@@ -0,0 +1,16 @@
+#pragma once
+
+class Selector
+{
+private:
+	int fila, columna;
+public:
+	Selector();
+	void inicializa();
+	void mover(unsigned char key);
+	int getFila();
+	int getColumna();
+	void reinicio();
+	void setPos(int _fila, int _columna);
+	
+};
