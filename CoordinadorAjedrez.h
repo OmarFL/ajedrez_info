1c6999fa74bbd1a2d3fa5a3398459fb2a9998a4a
AdrianPaiT <99014434+AdrianPaiT@users.noreply.github.com>
2025-06-14 23:32:42 +0200

    Update CoordinadorAjedrez.h

--- CoordinadorAjedrez.h
+++ CoordinadorAjedrez.h
@@ -3,8 +3,13 @@
 #include "ETSIDI.h"
 
 
-class coordinador
-{
+class coordinador {
+    Mundo mundo;        // Objeto que representa el mundo/entorno del juego
+    
 public:
-	void dibuja();
+
+    void dibuja();      // Método para renderizar/dibujar el juego
+    void tecla(unsigned char key);  // Maneja eventos de teclado
+    void Boton_Raton(int num_rival, int x, int y, int boton, bool abajo, bool espacio, bool ref_tecla);  // Maneja eventos del ratón
+
 };
