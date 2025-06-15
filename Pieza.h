1faf47c4a7fc3657ca46be7b9630513c57969f8e
Gallego1502 <80354497+Gallego1502@users.noreply.github.com>
2025-06-15 11:05:21 +0200

    Update Pieza.h

--- Pieza.h
+++ Pieza.h
@@ -1,43 +1,18 @@
 #pragma once
 #include "Vector.h"
-#include "Casilla.h"
-#include "ETSIDI.h"
+#include <ETSIDI.h>
 
 class Pieza
 {
-private:
-	/*
-	Vector posicion;
-	int estado,color;
-	Casilla fc;
+	int valor;              // Valor numérico de la pieza (para lógica de juego/IA)
+	Vector posicion_selecc; // Posición actual de la pieza en el tablero (coordenadas)
 
-	//Declaracion de los diferentes figuras
-	Sprite torre_w{ "imagenes/torre_w.png", 5 };
-	Sprite torre_b{ "imagenes/torre_b.png", 5 };
-	Sprite caballo_w{ "imagenes/caballo_w.png", 5 };
-	Sprite caballo_b{ "imagenes/caballo_b.png", 5 };
-	Sprite alfil_w{ "imagenes/alfil_w.png", 5 };
-	Sprite alfil_b{ "imagenes/alfil_b.png", 5 };
-	Sprite rey_w{ "imagenes/rey_w.png", 5 };
-	Sprite rey_b{ "imagenes/rey_b.png", 5 };
-	Sprite dama_w{ "imagenes/dama_w.png", 5 };
-	Sprite dama_b{ "imagenes/dama_b.png", 5 };
-	Sprite peon_w{ "imagenes/peon_w.png", 5 };
-	Sprite peon_b{ "imagenes/peon_b.png", 5 };
-	Sprite arzobispo_w{ "imagenes/arzobispo_w.png", 5 };
-	Sprite arzobispo_b{ "imagenes/arzobispo_b.png", 5 };
-	Sprite canciller_w{ "imagenes/canciller_w.png", 5 };
-	Sprite canciller_b{ "imagenes/canciller_b.png", 5 };
-	
 public:
-	virtual void dibuja();
-	virtual void setPos(int _f, int _c);
-	virtual int getTipoPieza();
-	int getColor();
-	int getfila();
-	int getcolumna();
-	Casilla getCasilla();
+	//Constructor
+	Pieza(int x, int y, int val);	 
 
-*/
-	
+	void Set_Posicion(int, int);	//Establece una nueva posición para la pieza
+
+	Vector Get_Posicion();			//Obtiene la posición actual de la pieza
+
 };
