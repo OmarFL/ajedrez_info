a9994a7a059926a8f03a078ba12685d95d45fd5d
AdrianPaiT <99014434+AdrianPaiT@users.noreply.github.com>
2025-06-19 15:08:03 +0200

    Update Tablero.h

--- Tablero.h
+++ Tablero.h
@@ -49,5 +49,6 @@ public:
 	void Seleccionar_Pieza_1VS1(Vector origen);  // Selecciona pieza en coordenadas
 	void Mover_Pieza_1VS1(Vector destino);       // Mueve pieza a coordenadas
 	void inicializa(const int& tipojuego); // Inicializa tablero según modo de juego
+	void Auto_Mov(); 
 
 };
